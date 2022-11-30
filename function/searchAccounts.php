a60b7c0b4caa2d8362620e83e76c8f2cc8469fa0
Matteo Rovellini <teorove04@gmail.com>
2022-11-30 13:57:14 +0100

    Finita funzione searchUser()

--- function/searchAccounts.php
+++ function/searchAccounts.php
@@ -1,15 +1,20 @@
 <?php
     require '../config/connection.php';
-    //input del testo da cercare dato che non è più una funzione
-    $sql="SELECT * FROM accounts WHERE username LIKE '$text%'";
-    $result=$conn->query($sql);
-    if($result->num_rows>0) {
-        $data=array();
-        while ($row=$result->fetch_assoc()) {
-            $data[]=$row;
+
+    $text=$_POST['txtSearch'];
+
+    if (!empty($text)) {
+        $sql="SELECT * FROM accounts WHERE username LIKE '$text%' LIMIT 4";
+        $result=$conn->query($sql);
+    
+        if($result->num_rows>0) {
+            $data=array();
+            while ($row=$result->fetch_assoc()) {
+                $data[]=$row;
+            }
         }
+        echo json_encode($data);
     }
-    echo json_encode($data);
     
     $conn->close();
 ?>
--- scripts/searchUser.js
+++ scripts/searchUser.js
@@ -1,29 +1,31 @@
-// come aggiungere classe con spazi
-
 var div=document.getElementById("card");
 var txt=document.getElementById("txtSearch");
 var ul=document.createElement("ul");
 ul.classList.add("list-group");
+ul.classList.add("list-group-flush");
 div.appendChild(ul);
 
 txt.addEventListener("input", function() {
 
     var request=new XMLHttpRequest();
-    request.open('GET', '../function/searchAccounts.php');
-    request.send();
+    request.open('POST', '../function/searchAccounts.php');
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var value=txt.value;
+    request.send("txtSearch="+value);
     
     request.onload = () => {
+        
         var response=JSON.parse(request.responseText);
 
         ul.replaceChildren();
-
         response.forEach(element => {
             var li=document.createElement("li");
             li.classList.add("list-group-item");
+            li.classList.add("text-bg-dark");
             ul.appendChild(li);
 
             var a=document.createElement("a");
-            a.href="#";
+            a.href="../public/profile.php";
             a.classList.add("link");
             li.appendChild(a);
 
@@ -37,9 +39,19 @@ txt.addEventListener("input", function() {
             span.classList.add("white");
             span.innerHTML=element["username"];
             a.appendChild(span);
-        });
-        
 
+            var divBorder=document.createElement("div");
+            divBorder.classList.add("bottom-border");
+            ul.appendChild(divBorder);
+        });
     }
+  
     
+});
+
+
+txt.addEventListener('keyup',function(){
+    if(txt.value.length == 0){
+       ul.replaceChildren();
+    }
 });
