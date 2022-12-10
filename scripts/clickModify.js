a7e6bfd56c042694835ba1efaa0966957344cd82
Federico Petrera <116896028+pytaoffi@users.noreply.github.com>
2022-12-10 18:13:14 +0100

    Add files via upload

---
+++ scripts/clickModify.js
@@ -0,0 +1,9 @@
+var mod=document.getElementById('btnModProfile');
+
+var button=document.getElementById('edit-profile');
+button.addEventListener('click',function(){
+    //todo lo sa elio
+    mod.click();
+})
+
+
