75deb61be90ddd6e963e0b57b50b9599b6860893
SimoneFornoni <104772701+SimoneFornoni@users.noreply.github.com>
2022-12-10 15:42:47 +0100

    Add files via upload

--- public/post.php
+++ public/post.php
@@ -22,15 +22,14 @@
     <div class="col-2">
       <aside class="no-overflow position-absolute top-0 start-0 sticky-bottom px-4" id="side-nav">
         <div class="header-sidebar">
-          <h1 class="fs-4 text-center mb-4"><span><img src="Images\Logo_FARP3.png" alt="Farp_Logo" id="img-logo" onclick="this.src='Images/colorful.PNG'"></span><span class="text-white">Farpgram</span></h1>
-        </div>
+        <h1 class="fs-4 text-center mb-4"><span><img src="Images\Logo_FARP3.png" alt="Farp_Logo" id="img-logo" onclick="this.src='Images/Logo_FARP3_dead.png'"></span><span class="text-white">Farpgram</span></h1>        </div>
         <ul id="functions">
-          <li class="function my-4"> <span> <i class="fa-solid fa-house text-white"></i> </span><a class="text-white text-decoration-none" href="home.php"> <span class="link">HOME</span> </a> </li>
-          <li class="function my-4"> <span> <i class="fa-solid fa-magnifying-glass text-white"> </i> </span><a class="text-white text-decoration-none" data-bs-toggle="offcanvas" href="#offcanvas-cerca" role="button" aria-controls="offcanvas-cerca"> <span class="link">CERCA</span> </a> </li>
-          <li class="function my-4"> <span> <i class="fa-solid fa-bell text-white"></i> </span><a class="text-white text-decoration-none" href="notifications.php"> <span class="link">NOTIFICHE</span> </a> </li>
-          <li class="function my-4"> <span> <i class="fa-solid fa-square-plus text-white"></i> </span><a class="text-white text-decoration-none" data-bs-toggle="offcanvas" href="#offcanvas-pubblica" role="button" aria-controls="offcanvas-pubblica"> <span class="link">PUBBLICA</span> </a> </li>
-          <li class="function my-4"> <span> <i class="fa-solid fa-user text-white"></i> </span><a class="text-white text-decoration-none" href="myprofile.php"> <span class="link">PROFILO</span> </a> </li>
-          <li class="function my-4">
+          <li class="function my-4 ms-5"> <span> <i class="fa-solid fa-house text-white"></i> </span><a class="text-white text-decoration-none" href="home.php"> <span class="link">HOME</span> </a> </li>
+          <li class="function my-4 ms-5"> <span> <i class="fa-solid fa-magnifying-glass text-white"> </i> </span><a class="text-white text-decoration-none" data-bs-toggle="offcanvas" href="#offcanvas-cerca" role="button" aria-controls="offcanvas-cerca"> <span class="link">CERCA</span> </a> </li>
+          <li class="function my-4 ms-5"> <span> <i class="fa-solid fa-bell text-white"></i> </span><a class="text-white text-decoration-none" href="notifications.php"> <span class="link">NOTIFICHE</span> </a> </li>
+          <li class="function my-4 ms-5"> <span> <i class="fa-solid fa-square-plus text-white"></i> </span><a class="text-white text-decoration-none" data-bs-toggle="offcanvas" href="#offcanvas-pubblica" role="button" aria-controls="offcanvas-pubblica"> <span class="link">PUBBLICA</span> </a> </li>
+          <li class="function my-4 ms-5"> <span> <i class="fa-solid fa-user text-white"></i> </span><a class="text-white text-decoration-none" href="myprofile.php"> <span class="link">PROFILO</span> </a> </li>
+          <li class="function my-4 ms-4">
             <div class="btn dropup">
               <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Altro
@@ -56,8 +55,8 @@
               <div class="d-flex justify-content-end">
                 </div>
                 <a class="navbar-brand" href="myprofile.php">
-                  <span class="text-white"><?php echo $_SESSION['user'] ?></span> 
                   <img src="Images\Icons\Defalut_pfp.png" alt="Logo" width="50" class="d-inline-block">
+                  <span class="text-white"><?php echo $_SESSION['user'] ?></span> 
                 </a>   
             </div>
           </nav>
@@ -66,21 +65,24 @@
               <div class="text-start card text-bg-dark mb-3 d-inline-flex" style="width: 25rem">
                 <div class="card-body">
                   <img src="Images\colorful.PNG" class="d-inline rounded-circle profile-image">
-                  <h5 class="card-title d-inline"> <a href="profile.php" class="accountToSearch text-decoration-none link">Mattehxx</a> </h5>
-                  <p class="card-text">luogo</p>
+                  <h5 class="card-title d-inline"> <a href="profile.php" class="accountToSearch text-decoration-none link" id="user"> </a></h5>
+                  <p class="card-text" id="location"> </p>
                 </div>
-                <img src="Images\Screenshot (5).png" class="card-img-top" alt="caricamento in corso">
+                <img src="Images\Screenshot (5).png" class="card-img-top" id="img" alt="caricamento in corso">
                 <div class="card-body">
-                  <p class="card-text">isisbernocchi</p>
-                  <p class="grey"> 01/12/2022</p>
+                  <a href="#"><i class="fa-regular fa-heart text-white"></i></a> <!-- <i class="fa-solid fa-heart"></i> -->
+                  <a href="post.php"><i class="fa-regular fa-comment text-white"></i></a>
+                  <p class="card-text" id="descrizione"> </p>
+                  <p class="grey" id="ts"> </p>
                 </div>              
               </div>
               <div class="text-start card text-bg-dark mb-3 d-inline-flex" style="width: 25rem">
-              <div class="card-body comments">
+              <div class="card-body">
                   <h3 class="text-center">COMMENTI</h3>
                   <hr class="border-black">
                 </div>
-                <div class="overflow-auto" style="max-height: 200px">
+                <input type="text" name="comment" class="form-control rounded-pill text-bg-dark mb-3" id="comment" placeholder="inserisci il tuo commento...">
+                <div class="overflow-auto comments" style="max-height: 180px">
                   <ul style="list-style: none">
                     <li> <img src="Images\colorful.PNG" class="d-inline rounded-circle profile-image"> <a href="profile.php" class="accountToSearch text-decoration-none link">Marco</a> : <span class="comment-text text-break" > il mio commento </span> </li>
                     <li> <img src="Images\colorful.PNG" class="d-inline rounded-circle profile-image"> <a href="profile.php" class="accountToSearch text-decoration-none link">Mattehxx</a> : <span class="comment-text text-break" > il mio commento </span> </li>
@@ -97,15 +99,20 @@
   <!-- OFFCANVAS-PUBBLICA -->
   <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvas-pubblica" aria-labelledby="offcanvas-pubblica-Label">
     <div class="offcanvas-header">
-      <h5 class="offcanvas-title my-5" id="offcanvas-pubblica-Label">Crea un nuovo post</h5>
+      <h5 class="offcanvas-title mt-5" id="offcanvas-pubblica-Label">Crea un nuovo post</h5>
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
       <form action="myfile.php">
+        <input type="file" class="form-control text-bg-dark my-4" name="img" accept="image/*" required>
         <label for="description">Inserisci descrizione del post</label>
-        <input type="text" class="form-control rounded-pill text-bg-dark my-3" name="description" id="description">
-        <input type="file" class="form-control text-bg-dark my-5" name="image" accept="image/*" required>
-        <input type="submit" class="btn btn-primary">
+        <textarea name="description" id="description" class="bg-dark text-white rounded mb-4" cols="45" rows="10"></textarea>
+        <label for="text-field">Inserisci luogo</label>
+        <input type="text" name="text-field" id="text-field" class="form-control bg-dark text-white mb-4">
+        <div id="locations">
+          <!-- quí vengono inseriti i luoghi -->
+        </div>
+        <input type="submit" name="submit" class="btn btn-primary text-center">
       </form>
     </div>
   </div>
