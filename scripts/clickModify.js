bce1833ef43d0e0bb34bce8511d65f10dd4bb885
SimoneFornoni <104772701+SimoneFornoni@users.noreply.github.com>
2022-12-12 18:13:14 +0100

    Delete clickModify.js

--- scripts/clickModify.js
+++
@@ -1,9 +0,0 @@
-var mod=document.getElementById('btnModProfile');
-
-var button=document.getElementById('edit-profile');
-button.addEventListener('click',function(){
-    //todo lo sa elio
-    mod.click();
-})
-
-
